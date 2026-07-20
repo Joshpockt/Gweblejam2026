126e889c57aca5f7dd5ba1bc5d5939d7af57d027
BurntToast <steven@jasonsmith.net>
2026-07-20 09:51:26 -0500

    Update menu.gd

--- menu.gd
+++ menu.gd
@@ -2,8 +2,15 @@ extends Node
 
 @onready var play: Button = $UI/Play
 @onready var exit: Button = $UI/exit
+var game = preload("res://ship.tscn")
 
+func playGame():
+	queue_free()
+	get_tree().root.add_child(game.instantiate())
+	
+	
 
 func _ready() -> void:
+	play.pressed.connect(playGame)
 	exit.pressed.connect(func():
 		get_tree().quit())
